306c76e901ed996f70d12bba610eb26b0b8c3160
cherinckx14 <39385152+cherinckx14@users.noreply.github.com>
2020-12-04 16:34:26 -0800

    Adding Handlebars functionality

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
 // server.js
+var path = require('path')
+var express = require('express');
+var exphbs = require('express-handlebars');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res, next) {
+    res.status(200).render('index.html', {
+    });
+  });
+
+app.get('*', function (req, res) {
+    res.status(404).render('404', {
+  
+    });
+  });
+
+app.listen(port, function () {
+    console.log("== Server is listening on port", port);
+  });
