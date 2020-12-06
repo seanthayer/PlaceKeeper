7241a67f2bc3af62d34a862dd7553faee96f0f98
Sean Thayer <thayerfs@oregonstate.edu>
2020-12-05 16:20:47 -0800

    rename var 'long' => 'lng'

--- public/map.js
+++ public/map.js
@@ -6,7 +6,7 @@ script.defer = true;
 // The API will callback when finished loading
 window.initMap = function() {
   // Creates a new Map object inserting it into <div id="map"></div>
-  map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById('map'), {
     // Each Map object must have a center defined with a latitude & longitude pair, and a zoom level
     center: {lat: 43.815136416911436, lng: -120.6398112171833},
     zoom: 5
