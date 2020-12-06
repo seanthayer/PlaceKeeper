489501d943232596ba57f53743252834dd16397a
Sean Thayer <thayerfs@oregonstate.edu>
2020-12-05 18:34:14 -0800

    add basic map importing framework

--- public/map.js
+++ public/map.js
@@ -2,26 +2,50 @@ var map;
 
 // The API will callback 'initMap()' when finished loading
 function initMap() {
+
   // Creates a new Map object inserting it into <div id="map"></div>
   map = new google.maps.Map(document.getElementById('map'), {
+
     // Each Map object must have a center defined with a latitude & longitude pair, and a zoom level
     center: {lat: 43.815136416911436, lng: -120.6398112171833},
     zoom: 5
+
   });
+
+  // importMap();
+
+}
+
+function importMap() {
 
   var getRequest = new XMLHttpRequest();
   var reqURL = '/importMap';
+
   getRequest.open('GET', reqURL);
 
   getRequest.setRequestHeader('Content-Type', 'application/json');
 
   getRequest.addEventListener('load', function(event) {
-    console.log("== Client side:");
-    console.log(event);
-    console.log(event.target.response);
+
+    var importMap;
+
+    importMap = JSON.parse(event.target.response);
+
+    importMap.forEach((item, i) => {
+
+      var coords = { lat: parseFloat(item.lat), lng: parseFloat(item.lng) };
+
+      var marker = new google.maps.Marker({
+
+        position: coords,
+        map: map
+
+      });
+
+    });
+
   });
 
   getRequest.send();
 
-  map.data.loadGeoJson('places-to-go.json');
 }
