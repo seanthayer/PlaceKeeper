7d7bf5f60c6d4265dfb523392496a222caccbd9f
Sean Thayer <seantf96@gmail.com>
2021-05-19 15:54:26 -0700

    complete homepage render

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ function App() {
 
       </header>
 
-      <section className="map-container">
-        <Map />
-      </section>
+      <main>
+        <div className="content-container">
+          <Map />
+          <PlacesList />
+        </div>
+      </main>
 
     </div>
   );
@@ -25,7 +28,54 @@ function App() {
 class Map extends React.Component {
   render() {
     return (
-      <div id="map"></div>
+      <section className="map-container">
+        <div id="map"></div>
+
+        <div className="import-and-save-buttons-container">
+
+        <div className="import-map-element">
+          <button type="button" className="import-map-button">Import Map</button>
+        </div>
+
+        <div className="save-map-element">
+          <button type="button" className="save-map-button">Save Map</button>
+        </div>
+
+        </div>
+      </section>
+    );
+  }
+}
+
+class PlacesList extends React.Component {
+  render() {
+    return (
+      <aside className="saved-places-container">
+
+        <div className="search-bar-container">
+          <div className="search-bar-element">
+            <input type="text" name="filter-text" className="search-bar-input" />
+          </div>
+          <button className="search-bar-button">Search</button>
+        </div>
+
+        <div className="saved-places-list-parent">
+
+          <h5 className="saved-places-list-title">Saved Places</h5>
+
+          <div className="saved-places-filter-info hidden">
+
+          </div>
+
+          <div className="saved-places-list-container">
+            <ul className="saved-places-list-element">
+
+            </ul>
+          </div>
+
+        </div>
+
+      </aside>
     );
   }
 }
