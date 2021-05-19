c17bc9f492e35b3b2f38a843e3418bad0cf401e8
Sean Thayer <seantf96@gmail.com>
2021-05-19 14:12:32 -0700

    basic API link & page styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import background from './img/background_header-bg.png';          // Sloppy,
+import header from './img/thumbnail_placekeeper-header-icon.png'; // Find a better way.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="PlaceKeeper">
+      <header className="PK-header" style={{backgroundImage: `url(${background})`}}>
+        
+        <h1 className="site-title">PlaceKeeper</h1>
+
+        <div className="header-image-container"><img className="header-image" src={`${header}`} alt="PlaceKeeper"/></div>
+
       </header>
+
+      <section className="map-container">
+        <Map />
+      </section>
+
     </div>
   );
 }
+
+class Map extends React.Component {
+  render() {
+    return (
+      <div id="map"></div>
+    );
+  }
+}
+
+
+// EXAMPLE CODE, disregard
+/*
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>A Simple Clock</h1>
+        <h2>
+          It is {this.state.date.toLocaleTimeString()}
+        </h2>
+      </div>
+    );
+  }
+  
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+}
+*/
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: '',
+  version: 'weekly',
+  clickableIcons: false
+});
+
+loader.load().then(() => {
+  const google = window.google;
+  let mapEmbed;
+
+  mapEmbed = new google.maps.Map(document.querySelector('#map'), {
+
+    center: { lat: 43.815136416911436, lng: -120.6398112171833 },
+    zoom: 5,
+    clickableIcons: false
+
+  });
+});
 
 ReactDOM.render(
   <React.StrictMode>
